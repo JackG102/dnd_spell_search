e8947802ad7bab276aa8e9e8eb09e37249d0e2de
Jack Garratt <jack.garratt@icfnext.com>
2021-11-29 18:04:06 -0500

    Coded accordion header and added some styling

--- src/components/ApiAccordion/ApiAccordion.js
+++ src/components/ApiAccordion/ApiAccordion.js
@@ -1,10 +1,13 @@
+import './ApiAccordion.css';
 import React from 'react';
+import axios from 'axios';
 
 const ApiAccordion = ({spell}) => {
   return(
     <div className="api-accordion">
       <div className="api-accordion--header">
         <span>{spell.name}</span>
+        <button className="ui button green">Read More</button>
       </div>
       <div className="api-accordion--body">
 
