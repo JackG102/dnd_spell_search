ec902a83abf94601eefb78385ce5269a17ff660b
Jack Garratt <jack.garratt@icfnext.com>
2021-11-29 12:39:33 -0500

    Added more conditional rendering and making api fetch call with axious

--- src/App.js
+++ src/App.js
@@ -7,21 +7,28 @@ const App = () => {
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
+    // For fun, create helper function
     const finishedLoading = () => {
       setLoading(false);
     }
-    setTimeout(() => {
-      finishedLoading();
-    }, 1000)
+
+    const search = async () => {
+      const { data } = await axios.get('https://www.dnd5eapi.co/api/spells');
+      // For fun, wait a moment so that I can practice conditional rendering.
+      // In reality, the api fetch call is really fast, so the loader only renders for a split second
+      // I want to practice and use my Loader component, hence the timeout function
+      setTimeout(() => {
+        finishedLoading();
+      }, 1000)
+      console.log(data);
+    }
+    search();
   }, []);
 
   return(
     <div className="app-container">
       <h1>DnD Search</h1>
-      {isLoading ?
-      <Loader />
-      :
-      <p>Done loading</p>}
+      {isLoading ? <Loader /> : <p>Done loading</p>}
     </div>
   );
 };
