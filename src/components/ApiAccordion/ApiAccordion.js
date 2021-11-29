d26f024d9888dd5a59ac0d6513f6335377ea7c3d
Jack Garratt <jack.garratt@icfnext.com>
2021-11-29 18:21:26 -0500

    Created initial api call for accrdion components

--- src/components/ApiAccordion/ApiAccordion.js
+++ src/components/ApiAccordion/ApiAccordion.js
@@ -1,16 +1,38 @@
 import './ApiAccordion.css';
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const ApiAccordion = ({spell}) => {
+
+  const [spellProperties, setSpellProperties] = useState([]);
+  const [isFetched, setIsFetched] = useState(false);
+
+  const fetchSpell = () => {
+    console.log('fun');
+    const fetchSpellProperties = async () => {
+      await axios.get('https://www.dnd5eapi.co/api/spells/' + spell.index).then(response => {
+        console.log(response);
+        setSpellProperties(response.data);
+        setIsFetched(true);
+      });
+    }
+
+    fetchSpellProperties();
+  }
+
   return(
     <div className="api-accordion">
       <div className="api-accordion--header">
         <span>{spell.name}</span>
-        <button className="ui button green">Read More</button>
+        <button 
+          className="ui button green"
+          onClick={fetchSpell}
+        >
+          Read More
+        </button>
       </div>
       <div className="api-accordion--body">
-
+        { isFetched ? <p>{spellProperties.desc}</p> : null }
       </div>
     </div>
   );
